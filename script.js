404bcb0e94f52a14acadaa6b62c7ea77aa3f63e0
amolnirphal <142665914+amolnirphal@users.noreply.github.com>
2026-06-14 22:42:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes.forEach((code, index) => {
+    code.addEventListener("input", (e) => {
+        code.value = code.value.slice(0, 1);
+
+        if (code.value !== "" && index < codes.length - 1) {
+            codes[index + 1].focus();
+        }
+    });
+
+    code.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace") {
+            if (code.value === "" && index > 0) {
+                codes[index - 1].value = "";
+                codes[index - 1].focus();
+            } else {
+                code.value = "";
+            }
+        }
+    });
+});
+
+codes[0].focus();
