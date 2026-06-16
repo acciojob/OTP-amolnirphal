67bb45e7ce34c99f116359c3daec3f2d6fadc687
amolnirphal <142665914+amolnirphal@users.noreply.github.com>
2026-06-16 23:42:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 //your JS code here. If required.
 const codes = document.querySelectorAll(".code");
 
+// Focus on the first input
 codes[0].focus();
 
 codes.forEach((code, index) => {
-  code.addEventListener("input", () => {
-    if (code.value.length > 1) {
-      code.value = code.value.slice(0, 1);
-    }
+
+  // Move to next box after typing
+  code.addEventListener("input", (e) => {
+    code.value = code.value.slice(0, 1);
 
     if (code.value !== "" && index < codes.length - 1) {
       codes[index + 1].focus();
     }
   });
 
+  // Handle backspace
   code.addEventListener("keydown", (e) => {
     if (e.key === "Backspace") {
-      code.value = "";
-
-      if (index > 0) {
-        setTimeout(() => {
-          codes[index - 1].focus();
-        }, 0);
+      if (code.value === "" && index > 0) {
+        codes[index - 1].focus();
+      } else {
+        code.value = "";
       }
     }
   });
